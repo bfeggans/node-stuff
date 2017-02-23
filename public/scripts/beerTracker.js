31abd5e485f72a8d608503d7ab6d515a97de04c2
Blake Feggans <bfeggans@gmail.com>
2017-02-22 22:29:17 -0800

    added functionality to change and update ratings on tracked beers

--- public/scripts/beerTracker.js
+++ public/scripts/beerTracker.js
@@ -2,9 +2,30 @@
   var addButton = document.getElementById('addBeerButton');
   var beerList = document.getElementById('beerList');
   //todo: find out where to put these transformable env variables
-  var apiURI = 'https://shielded-coast-63607.herokuapp.com';
-  // var apiURI = 'http://localhost:5000';
+  // var apiURI = 'https://shielded-coast-63607.herokuapp.com';
+  var apiURI = 'http://localhost:5000';
 
+  document.addEventListener('click', (evt) => {
+    if (evt.target.parentNode.className
+        && evt.target.parentNode.className.indexOf('tracked-beer-rating-btn') > -1) {
+      evt.target.parentNode.nextSibling.style = '';
+    } else {
+      const selects = document.getElementsByTagName('select');
+
+      Array.prototype.forEach.call(selects, (element) => {
+        if (element.style === 'display: none;' || element.id === 'beerRating') return;
+        element.style = 'display: none;';
+      });
+    }
+  });
+  document.addEventListener('change', (evt) => {
+    if (evt.target.className === 'tracked-beer-rating') {
+      const newRating = evt.target.value;
+
+      evt.target.previousSibling.childNodes[1].textContent = newRating;
+      evt.target.style = 'display: none;';
+    }
+  });
   document.addEventListener('DOMContentLoaded', getBeerList);
   beerList.addEventListener('click', function(evt) {
     if (evt.target.className.indexOf('fa-close') > -1) {
@@ -16,8 +37,10 @@
     } else if (evt.target.className.indexOf('update-beer') > -1) {
       const ajax = liteAjax('PUT', `${apiURI}/beerTracker/api/updateBeer/${evt.target.parentNode.id}`);
       const beerName = evt.target.parentNode.getElementsByTagName('input')[0].value;
+      const beerRating = evt.target.previousSibling.childNodes[1].textContent;
       const postObj = JSON.stringify({
-        name: beerName
+        name: beerName,
+        rating: beerRating
       });
 
       ajax({
@@ -88,11 +111,15 @@
   function buildBeerCard(beer) {
     var html = `<a class="remove-beer"><i class="fa fa-close"></i></a>
                 <input type="text" value="${beer.name}" class="tracked-beer-name" /> <br>
-                <span class="fa-stack">
-                  <i class="fa fa-star fa-stack-2x"></i>
-                  <strong class="fa-stack-1x rating-text">${beer.rating}</strong>
-                </span>
-                <a class="update-beer btn btn-primary">Update</a>`;
+                <div style="position: relative; display: inline-block;">
+                  <button class="fa-stack tracked-beer-rating-btn" type="button"><i class="fa fa-star fa-stack-2x"></i><strong class="fa-stack-1x rating-text">${beer.rating}</strong></button><select size="5" name="rating" class="tracked-beer-rating" style="display: none;">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                </div><a class="update-beer btn btn-primary">Update</a>`;
 
     return html;
   }
